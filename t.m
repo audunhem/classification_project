8ca53fd2dbc8eef14c32cf9910ac34dcae69b696
monslaup <monslaup@gmail.com>
2019-04-01 11:55:02 +0200

    Update t.m

--- t.m
+++ t.m
@@ -1,11 +1,12 @@
-function [train_set, test_set]= t(vow_n)
+function [train, test]= t(vow_n)
 vowels;
 n = find(vowel_code==vow_n); 
-
 set = zeros(139, 14);
+
 for i = n(1):n(size(n,2))
     set(i-(n(1)-1),:) = all_data(i,:);
 end
-train_set = set(1:70, :);
-test_set = set(71:139, :);
+
+train = set(1:70, :);
+test = set(71:139, :);
 end
