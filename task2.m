54ee2dceba0a2691f01a1d95d0f2be861c00451f
Audun <audunhem@gmail.com>
2019-04-16 18:12:02 +0200

    Update task2.m

--- task2.m
+++ task2.m
@@ -1,45 +1,60 @@
+%% Initalization of variables, data and distributions
+
 training_data = [];
 test_data = [];
+n_formants = 14; %variable to enable use of a subset of formants
 
+%get the testing and training data for all vowels
 for i = 1:12
-    [train, test] = t(i);
-    training_data(:,:,i) = train/3000;
-    test_data(:,:,i) = test/3000;
+    [train, test] = vowel_data(i);
+    training_data(:,:,i) = train;
+    test_data(:,:,i) = test;
 end
 
-GMM = cell(1,12);
-M = 3;
+%example of how to use only a subset of the formants (here n_formants = 13)
+%training_data = training_data(:,[1:5 7:14],:);
+%test_data = test_data(:,[1:5 7:14],:);
 
+GMMs = cell(1,12,10); %declaring an empty call array to hold the GMMs 
+M = 2; %the number of Gaussians in each GMM
+
+
+%filling the cell array with a GMM for each vocal
 for i = 1:12
-    GMM{i} = fitgmdist(training_data(:,:,i),M,'Options',statset('MaxIter',1500,'TolFun',1e-50),'Regularize', 1e-10,'CovarianceType','diagonal','ProbabilityTolerance',1e-40,'Replicates',10);
-    GMM{i}.NumIterations
+    GMMs{i} = fitgmdist(training_data(:,:,i),M,'Regularize', 1e-5,'CovarianceType','diagonal');
 end
 
-errors_full = 0;
+%% Classification of the vocals
+
+%creating the confusion matrix
 for i = 1:12
-    confm_full(i,:) = classify_vocal_GMM(test_data(:,:,i),GMM);
-    errors_full = errors_full + sum(confm_full(i,:))-confm_full(i,i);
+    confm(i,:) = classify_vocal(test_data(:,:,i),GMMs);
 end
 
-error_rate_full = errors_full/(69*12)
-    
+%computing the error rate
+errors = sum(confm,'all')-sum(confm.*eye(12),'all');
+error_rate = errors/(69*12)
 
-function confm_row = classify_vocal_GMM(test_set,GMM)
+%returning a row of the confusion matrix for a given vocal
+function confm_row = classify_vocal(test_set,GMM)
     confm_row = zeros(12,1);
     for i = 1:69
-        class = classify_sample_GMM(test_set(i,:),GMM);
+        class = classify_sample(test_set(i,:),GMM);
         confm_row(class) = confm_row(class) + 1;
     end
 end
 
-function class = classify_sample_GMM(x,GMM)
+%returning the predicted class for a vocal sample
+function class = classify_sample(x,GMM)
     probabilities = zeros(1,12);
     for i = 1:12
+        %iterating over all the vocal distributions
         probabilities(i) = 0;
         for j = 1:GMM{i}.NumComponents
             probabilities(i) = probabilities(i) + GMM{i}.ComponentProportion(j)*mvnpdf(x,GMM{i}.mu(j,:),GMM{i}.Sigma(:,:,j));
         end
     end
+    %returning the index of the maximum probability
     [~,class] = max(probabilities);
 end
 
